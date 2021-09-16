a003be3b1486d12b22b042e4e3b26b0e2852f305
bwty <whalelephant@users.noreply.github.com>
2021-09-17 00:40:01 +0800

    fix: gha typo

--- contracts-test.sh
+++ contracts-test.sh
@@ -14,8 +14,8 @@ set -ex
 OS_NAME=$(uname -s | awk '{print tolower($0)}')
 CPU_ARCH=$(uname -m)
 PROJECT_ROOT=$(pwd)
-#EXEC_PATH="$PROJECT_ROOT"/.github/flow-"$OS_NAME"-"$CPU_ARCH"
-EXEC_PATH=/usr/local/bin/flow
+EXEC_PATH="$PROJECT_ROOT"/.github/flow-"$OS_NAME"-"$CPU_ARCH"
+# EXEC_PATH=/usr/local/bin/flow
 
 shopt -s expand_aliases
 alias flow='$EXEC_PATH -f $PROJECT_ROOT/flow.json'
