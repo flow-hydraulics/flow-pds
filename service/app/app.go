ba33650ce8bd635c9b1a82d6173f87881ad0b5d6
Lauri Junkkari <latenssi@gmail.com>
2021-11-25 15:23:58 +0200

    Separate batch sizes for insert and process

--- service/app/app.go
+++ service/app/app.go
@@ -59,7 +59,7 @@ func (app *App) CreateDistribution(ctx context.Context, distribution *Distributi
 		return err
 	}
 
-	if err := InsertDistribution(app.db, distribution, app.cfg.QueryBatchSize); err != nil {
+	if err := InsertDistribution(app.db, distribution, app.cfg.BatchInsertSize); err != nil {
 		return err
 	}
 
--- service/config/config.go
+++ service/config/config.go
@@ -39,8 +39,9 @@ type Config struct {
 	SettlementBatchSize int `env:"FLOW_PDS_SETTLEMENT_BATCH_SIZE" envDefault:"40"`
 	MintingBatchSize    int `env:"FLOW_PDS_MINTING_BATCH_SIZE" envDefault:"40"`
 
-	// The batchSize for database batch handling (big inserts or batch processing)
-	QueryBatchSize int `env:"FLOW_PDS_QUERY_BATCH_SIZE" envDefault:"1000"`
+	// The batch sizes for database batch handling (big inserts or batch processing)
+	BatchInsertSize  int `env:"FLOW_PDS_BATCH_INSERT_SIZE" envDefault:"1000"`
+	BatchProcessSize int `env:"FLOW_PDS_BATCH_PROCESS_SIZE" envDefault:"1000"`
 
 	// -- Testing --
 
